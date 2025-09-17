eb6284c7b6fd31c94375d12045decfd487cc60c4
civico55 <52667089+civico55@users.noreply.github.com>
2025-09-17 16:54:55 +0200

    Add files via upload

---
+++ menuSettimanale.js
@@ -0,0 +1,52 @@
+
+export const menuSettimanale = {
+  'Lunedì': {
+    'Colazione': 'Caffè + 2 gallette con marmellata + mandorle',
+    'Spuntino': '1 frutto + 2 noci',
+    'Pranzo': 'Riso con zucchine + bresaola + verdure',
+    'Merenda': 'Yogurt naturale + frutta',
+    'Cena': 'Pesce al forno + patate lesse + verdura'
+  },
+  'Martedì': {
+    'Colazione': 'Latte vegetale + cereali integrali',
+    'Spuntino': 'Mandorle + mela',
+    'Pranzo': 'Farro con verdure + tonno al naturale',
+    'Merenda': 'Smoothie di frutta',
+    'Cena': 'Pollo ai ferri + verdure grigliate'
+  },
+  'Mercoledì': {
+    'Colazione': 'Tè verde + fette biscottate integrali + miele',
+    'Spuntino': 'Banana + 3 noci',
+    'Pranzo': 'Pasta integrale con pomodorini + ricotta',
+    'Merenda': 'Frutta fresca',
+    'Cena': 'Uova strapazzate + insalata mista + pane integrale'
+  },
+  'Giovedì': {
+    'Colazione': 'Yogurt greco + frutta secca + avena',
+    'Spuntino': 'Frutta a scelta',
+    'Pranzo': 'Cous cous con ceci e verdure',
+    'Merenda': 'Carote + hummus',
+    'Cena': 'Filetto di manzo + broccoli al vapore'
+  },
+  'Venerdì': {
+    'Colazione': 'Caffè + pancake di avena con miele',
+    'Spuntino': 'Macedonia + semi oleosi',
+    'Pranzo': 'Insalatona con quinoa + legumi + semi',
+    'Merenda': 'Frutta secca + tisana',
+    'Cena': 'Salmone alla griglia + verdure al vapore'
+  },
+  'Sabato': {
+    'Colazione': 'Latte + biscotti integrali',
+    'Spuntino': 'Frutto + mandorle',
+    'Pranzo': 'Lasagna vegetariana',
+    'Merenda': 'Gelato alla frutta',
+    'Cena': 'Pizza margherita + insalata'
+  },
+  'Domenica': {
+    'Colazione': 'Cappuccino + cornetto integrale',
+    'Spuntino': 'Spremuta + noci',
+    'Pranzo': 'Arrosto di tacchino + contorno di verdure',
+    'Merenda': 'Dolce fatto in casa',
+    'Cena': 'Zuppa leggera + crostini integrali'
+  }
+};
